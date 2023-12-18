15806d8ad823ef755d5cd1a44c0a48002a0a3702
pabsantos <pabsantos21@gmail.com>
2023-12-18 13:41:35 -0300

    Cria e exporta os setores censitários de Curitiba

---
+++ R/main.R
@@ -0,0 +1,24 @@
+library(censobr)
+library(tidyverse)
+library(geobr)
+library(sf)
+
+tracts <- read_tracts(
+  year = 2010,
+  dataset = "DomicilioRenda",
+  as_data_frame = TRUE
+)
+
+cwb_tracts_data <- tracts |> 
+  filter(code_muni == 4106902)
+
+cwb_tracts <- read_census_tract(
+  year = 2010,
+  code_tract = 4106902
+)
+
+cwb_tracts_final <- cwb_tracts |> 
+  select(code_tract, name_muni, name_neighborhood) |> 
+  left_join(cwb_tracts_data, by = "code_tract")
+
+st_write(cwb_tracts_final, "data/cwb_tracts.gpkg")
